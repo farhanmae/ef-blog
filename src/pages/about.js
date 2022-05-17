8948244ba8b5c7470a282a55cc734abc059bac43
Farhan Erooth <farhan@tridz.com>
2022-05-18 02:42:26 +0530

    Blog Listing

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,12 +3,17 @@ import { Link } from 'gatsby'
 import { heading } from '../components/layout.module.css'
 
 import Layout from "../components/layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
     return(
         <Layout pageTitle="About Me">
             <h1 className={heading}>About Me</h1>
             <p>Hi There!</p>
+            <StaticImage
+                alt="startup"
+                src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            />
             <Link to="/">Go to home</Link>
         </Layout>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
-import { StaticImage } from 'gatsby-plugin-image'
 import { heading } from '../components/layout.module.css'
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <Layout pageTitle="Home">
       <h1 className={heading}>
         Farhan Erooth
       </h1>
-      <Link to="/about">About</Link>
-      <StaticImage
-        alt="startup"
-        src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-      />
+      <h2>My Blog Posts</h2>
+      <p>My cool posts will go in here</p>
+      <ul>
+        {
+          data.allFile.nodes.map(node => (
+            <li key={node.name}>{node.name}</li>
+          ))
+        }
+      </ul>
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
@@ -23,5 +26,15 @@ const IndexPage = () => {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default IndexPage
